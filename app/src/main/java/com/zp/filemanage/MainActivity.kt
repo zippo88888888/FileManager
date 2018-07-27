a80073852d8df87092fa92bd34f6adce6d1da309
zippo888888 <zp1025411146@163.com>
2018-07-27 11:58:54 +0800

    新增java调用示例

--- app/src/main/java/com/zp/filemanage/MainActivity.kt
+++ app/src/main/java/com/zp/filemanage/MainActivity.kt
@@ -28,13 +28,27 @@ class MainActivity : AppCompatActivity() {
 /*
         java调用
 
-        FileManageHelp.Companion.getInstance().start(this);
+        FileManageHelp.Companion.getInstance()
+                .setFileTypeListener(new MyFileTypeListener())
+                .setImgeLoad(new FileImageListener())
+                .setJumpListener(new MyJumpListener())
+                .setFileInfoListener(new FileInfoListener())
+                .setMaxLength(9, "最大选取数量：9")
+                .setCanRightTouch(true)
+                .setShowHiddenFile(false)
+                .setFileFilterArray(new String[]{PNG, JPG, GIF, MP3, AAC, MP4, _3GP, TXT, ZIP})
+                .setSortordByWhat(FileManageHelp.BY_DEFAULT)
+                .setSortord(FileManageHelp.ASC)
+                .setShowLog(true);
 
         FileManageHelp.Companion.getInstance().setFileResultListener(new Function1<ArrayList<FileBean>, Unit>() {
             @Override
             public Unit invoke(ArrayList<FileBean> list) {
+                Log.e("list", "size--->>>" + list.size());
                 return null;
             }
         });
+
+        FileManageHelp.Companion.getInstance().start(this);
 
 */
